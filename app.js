30e4ef112d56dcf2cd47124b7541a42030675533
June Hyok Cho <jcho0627@gmail.com>
2019-05-24 08:18:08 +0200

    user model and sign up page added

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const User = require("./models/user");
+const localStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://localhost/auth-app");
 
+const app = express();
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require("express-session")({
+  secret: "Rusty is the best and cutest dog in the world",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Read the session and taking the data and decoding/encoding it
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//================================
+//ROUTES
+//================================
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -12,6 +34,26 @@ app.get("/", (req, res) => {
 
 app.get("/secret", (req, res) => {
   res.render("secret");
+});
+
+// AUTH ROUTES
+
+// Show sign up form
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// Handle user sign up
+app.post("/register", (req, res) => {
+  User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/secret");
+    });
+  });
 });
 
 app.listen(3000, () => console.log("Server Started on Port : 3000"));
